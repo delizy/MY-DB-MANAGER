96c98ac4840b6b80f5039b47d33c61e53c7dea1b
delizy <delizyrebaz@gmail.com>
2026-04-01 01:06:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
   res.send('Server is running and connected to Neon Database!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
